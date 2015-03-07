ad9430073c9744cca95bf5178d670be18aced6d7
Joan Teixidó <joan@laiogurtera.com>
2015-03-07 17:46:31 +0100

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		bowercopy: {
+			options: {
+				srcPrefix: 'bower_components',
+				destPrefix: 'web/assets'
+			 },
+			scripts: {
+				files: {
+					'js/jquery.js': 'jquery/dist/jquery.js',
+					'js/bootstrap.js': 'bootstrap/dist/js/bootstrap.js'
+				}
+		 	},
+			stylesheets: {
+				files: {
+					'css/bootstrap.css': 'bootstrap/dist/css/bootstrap.css',
+					'css/font-awesome.css': 'font-awesome/css/font-awesome.css'
+				}
+		 	},
+			fonts: {
+				files: {
+					'fonts': 'font-awesome/fonts'
+				}
+	       		}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-bowercopy');
+	grunt.registerTask('default', ['bowercopy']);
+};
